b51d0f45348d9db8470d5807c97c5ad39ddfb2f0
raxakk <phi.ri@gmx.de>
2015-06-15 12:30:50 +0200

    bugs, bugs bugs

--- server/NoobOnlineService/ejbModule/de/noob/entities/Comment.java
+++ server/NoobOnlineService/ejbModule/de/noob/entities/Comment.java
@@ -19,7 +19,7 @@ public class Comment implements Serializable {
 	
 	private String text;
 	
-	@OneToMany (mappedBy = "superComment", cascade = CascadeType.ALL)
+	@OneToMany (mappedBy = "superComment", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	private List<Comment> comments;
 	
 	@ManyToOne
